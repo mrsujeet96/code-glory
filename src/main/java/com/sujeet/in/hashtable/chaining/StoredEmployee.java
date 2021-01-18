d6518edc75de7090b5d54e002dcf1d92689c26be
mrsujeet96 <mrsujeet96@gmail.com>
2021-01-18 20:10:06 +0530

    Chaning Hashtable Implementation

---
+++ src/main/java/com/sujeet/in/hashtable/chaining/StoredEmployee.java
@@ -0,0 +1,15 @@
+package com.sujeet.in.hashtable.chaining;
+
+
+
+public class StoredEmployee {
+    public String key;
+    public Employee employee;
+
+    public StoredEmployee(String key, Employee employee) {
+        this.key = key;
+        this.employee = employee;
+    }
+
+
+}
