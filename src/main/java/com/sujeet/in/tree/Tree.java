88bf9c717d6510870562248e4f5dfcbb69f28a52
mrsujeet96 <mrsujeet96@gmail.com>
2021-01-19 17:16:08 +0530

    Tree Implementation

---
+++ src/main/java/com/sujeet/in/tree/Tree.java
@@ -0,0 +1,70 @@
+package com.sujeet.in.tree;
+
+public class Tree {
+    private TreeNode root;
+
+    public void insert(int value) {
+        if (root == null) {
+            root = new TreeNode(value);
+        } else {
+            root.insert(value);
+        }
+    }
+
+    public TreeNode get(int value) {
+        if (root != null) {
+            return root.get(value);
+        }
+        return null;
+    }
+
+    public void delete(int value) {
+        root = delete(root, value);
+    }
+
+    public TreeNode delete(TreeNode subTreeRoot, int value) {
+        if (subTreeRoot == null) {
+            return subTreeRoot;
+        }
+        if (value < subTreeRoot.getData()) {
+            subTreeRoot.setLeftChild(delete(subTreeRoot.getLeftChild(), value));
+        } else if (value > subTreeRoot.getData()) {
+            subTreeRoot.setRightChild((delete(subTreeRoot.getRightChild(), value)));
+        } else {
+            if (subTreeRoot.getLeftChild() == null) {
+                return subTreeRoot.getRightChild();
+            } else if (subTreeRoot.getRightChild() == null) {
+                return subTreeRoot.getLeftChild();
+            }
+            subTreeRoot.setData(subTreeRoot.getRightChild().min());
+
+            subTreeRoot.setRightChild(delete(subTreeRoot.getRightChild(), subTreeRoot.getData()));
+        }
+        return subTreeRoot;
+    }
+
+    public int min() {
+        if (root == null) {
+            return Integer.MIN_VALUE;
+        } else {
+            return root.min();
+        }
+    }
+
+    public int max() {
+        if (root == null) {
+            return Integer.MAX_VALUE;
+
+        } else {
+            return root.max();
+        }
+    }
+
+    public void traverseInOrder() {
+        if (root != null) {
+            root.traverseInOrder();
+        }
+    }
+
+
+}
