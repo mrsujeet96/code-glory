be75e4a3bac0ab6147eb8b988320b8d83ea31fe1
mrsujeet96 <mrsujeet96@gmail.com>
2021-05-01 19:53:28 +0530

    last commit

--- src/main/java/com/sujeet/in/leetcode/MinMove2.java
+++ src/main/java/com/sujeet/in/leetcode/MinMove2.java
@@ -2,6 +2,24 @@ package com.sujeet.in.leetcode;
 
 import java.util.Arrays;
 
+//Given an integer array nums of size n, return the minimum number of moves required to make all array elements equal.
+//
+//In one move, you can increment or decrement an element of the array by 1.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [1,2,3]
+//Output: 2
+//Explanation:
+//Only two moves are needed (remember each move increments or decrements one element):
+//[1,2,3]  =>  [2,2,3]  =>  [2,2,2]
+//Example 2:
+//
+//Input: nums = [1,10,2,9]
+//Output: 16
+
 public class MinMove2 {
 
     public static void main(String[] args) {
