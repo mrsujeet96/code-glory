60b845a3af3206892e90d9b649fc25d873d13f2c
mrsujeet96 <mrsujeet96@gmail.com>
2021-05-14 20:26:35 +0530

    last commit

--- src/main/java/com/sujeet/in/leetcode/LengthOfLastWord.java
+++ src/main/java/com/sujeet/in/leetcode/LengthOfLastWord.java
@@ -1,4 +1,18 @@
 package com.sujeet.in.leetcode;
+//Given a string s consists of some words separated by spaces, return the length of the last word in the string. If the last word does not exist, return 0.
+//
+//A word is a maximal substring consisting of non-space characters only.
+//
+//
+//
+//Example 1:
+//
+//Input: s = "Hello World"
+//Output: 5
+//Example 2:
+//
+//Input: s = " "
+//Output: 0
 
 public class LengthOfLastWord {
     public static void main(String[] args) {
